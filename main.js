7c188cc2d435fbed02802dc6de2768d1bbc63638
wahyuandhika <wahyuandhika1337@gmail.com>
2022-03-31 19:49:51 +0700

    improve & fix bugs + add new song (fine on the outside)

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function parseLrc(text, callback) {
         if (allTextLines[i].search(/^(\[)(\d*)(:)(.*)(\])(.*)/i) >= 0) // any line without the prescribed format wont enter this loop 
         {
             let line = allTextLines[i].match(/^(\[)(\d*)(:)(.*)(\])(.*)/i);
-            timestamps[i] = (parseInt(line[2]) * 60) + parseInt(line[4]); // will give seconds 
+            timestamps[i] = (parseFloat(line[2]) * 60) + parseFloat(line[4]);
             lyrics[i] = line[6]; //will give lyrics
         }
     }
@@ -50,6 +50,9 @@ function createAudio(source) {
 }
 
 function syncAudioWithLrc(audio, lyrics, timestamps) {
+    // not good
+    var lastLyricsIndex = -1
+    var onEndOfLyrics = false // will cause a bug if user go to previous timestamps
     const synchronizer = setInterval(() => {
         // minuteText.innerHTML = "Minutes = " + Math.floor(music.currentTime / 60) + "";
         // secondsText.innerHTML = "Seconds = " + Math.floor(music.currentTime % 60) + "";
@@ -57,12 +60,30 @@ function syncAudioWithLrc(audio, lyrics, timestamps) {
         if (!audio.paused && audio.currentTime > 0 && !audio.ended) {
             let index = 0
             for (sec of timestamps) {
-                if (audio.currentTime < sec) {
-                    if (lyrics[index-1]) {
+                // before first lyrics, don't do anything
+                if (audio.currentTime < timestamps[0]) {
+                    break
+                }
+
+                if (!onEndOfLyrics && audio.currentTime < sec) {
+                    if (index != lastLyricsIndex) {
+                        // update shown lyrics
                         displayLyrics.innerHTML = lyrics[index-1]
+                        lastLyricsIndex = index
+                    }
+                    index += 1
+                    break
+                } else if (index == timestamps.length-1 && audio.currentTime > sec) {
+                    if (!onEndOfLyrics) {
+                        // update shown lyrics
+                        displayLyrics.innerHTML = lyrics[index]
+                        lastLyricsIndex = index
+                        // give signal to not update anymore
+                        onEndOfLyrics = true
                     }
                     break
                 }
+
                 index += 1
             }
         }
@@ -75,21 +96,47 @@ function startsSong(title) {
     console.log(`new song: ${title}`)
 
     console.log("preparing audio ...")
-    let audio = createAudio(`./db/${title}.ogg`)
+    let audio = createAudio(`./db/${title}.mp3`)
     
     console.log("loading lyrics ...")
     loadLrc(`./db/${title}.lrc`, (lyrics, timestamps) => {
-        if (timestamps.length > 0) console.log("lyrics ready")
+        let lyricsReady = false
+        if (lyrics.length > 0 && lyrics.length == timestamps.length) {
+            lyricsReady = true
+            console.log("lyrics ready")
+        } else {
+            console.log("no lyrics!")
+        }
         
         console.log("playing audio ...")
         audio.play()
 
-        synchronizer = syncAudioWithLrc(audio, lyrics, timestamps)
-        console.log("audio & lyrics synchronized")
-        
+        window.onkeypress = function(event) {
+            if (event.which == 32) {
+                if (!audio.paused) {
+                    audio.pause()
+                    console.log("audio paused.")
+                } else if (audio.paused) {
+                    audio.play()
+                    console.log("audio continued.")
+                }
+            }
+        }
+
+        let synchronizer = null
+        if (lyricsReady) {
+            // clean undefined value
+            lyrics = lyrics.filter((el) => { return el !== undefined; });
+            timestamps = timestamps.filter((el) => { return el !== undefined; });
+
+            synchronizer = syncAudioWithLrc(audio, lyrics, timestamps)
+            console.log("audio & lyrics synchronized")
+        }
+
         audio.onended = function(){
-            clearInterval(synchronizer)
+            if (lyricsReady) clearInterval(synchronizer)
             displayLyrics.innerHTML = ""
+            window.onkeypress=(e)=>{} // clear pause event
             audio.remove()
             console.log(`song ${title} ended.`)
             blackhole()
@@ -97,7 +144,7 @@ function startsSong(title) {
     })
 
     // set wallpaper
-    document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.1)),url(db/${title}.jpg)`
+    document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.14),rgba(0, 0, 0, 0.145)),url("db/${title}.jpg")`
     console.log("background placed")
 }
 
@@ -108,12 +155,19 @@ var lastIndex = 0
 function blackhole() {
     if (songList.length > 0) {
         let index = Math.floor(Math.random() * songList.length);
-        lastIndex = index
-        let title = songList[index]
-        startsSong(title)
+        let currentTitle = songList[index]
+        let ogIndex = 0
+        for (ogTitle of allSongList) {
+            if (ogTitle == currentTitle) {
+                lastIndex = ogIndex
+                break
+            }
+            ogIndex += 1
+        }
+        startsSong(currentTitle)
         songList.splice(index, 1)
     } else if (songList.length == 0 && allSongList.length > 0){
-        songList = allSongList
+        songList = allSongList.slice() // copy
         if (allSongList.length > 1) {
             songList.splice(lastIndex, 1)
         }
@@ -125,7 +179,7 @@ let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
         songList = xhr.responseText.split(/\r\n|\n/)
-        allSongList = songList.slice()
+        allSongList = songList.slice() // copy
         if (allSongList.length > 0) {
             blackhole()
         } else {
